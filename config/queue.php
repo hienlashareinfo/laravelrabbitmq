39f110987c2a8042e3e6b2be00c9e6fe70a59771
shang9x123 <shang9x@gmail.com>
2024-06-21 15:18:55 +0700

    'complete'

--- config/queue.php
+++ config/queue.php
@@ -39,8 +39,8 @@ return [
                 [
                     'host' => env('RABBITMQ_HOST', '127.0.0.1'),
                     'port' => env('RABBITMQ_PORT', 5672),
-                    'user' => env('RABBITMQ_USER', 'admin'),
-                    'password' => env('RABBITMQ_PASSWORD', '55c4ef9d'),
+                    'user' => env('RABBITMQ_USER', ''),
+                    'password' => env('RABBITMQ_PASSWORD', ''),
                     'vhost' => env('RABBITMQ_VHOST', '/'),
                 ],
             ],
